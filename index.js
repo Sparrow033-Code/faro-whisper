aa3931f0fd7addd6c716b1da763af0e033b0a7cc
Sparrow033-Code <informatica@e-osendi.com>
2026-03-19 19:07:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ import { kadDHT } from '@libp2p/kad-dht';
 import { identify } from '@libp2p/identify';
 import { ping } from '@libp2p/ping';
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2';
-import { privateKeyFromProtobuf } from '@libp2p/crypto/keys';
+import { createFromProtobuf } from '@libp2p/peer-id-factory';
 import { fromString } from 'uint8arrays/from-string';
 
 async function startFaro() {
     const port = process.env.PORT || 10000;
 
-    let privateKey;
+    let peerId;
     if (process.env.FARO_KEY) {
         console.log('🗼 Cargando clave persistente de FARO_KEY...');
         try {
-            privateKey = await privateKeyFromProtobuf(fromString(process.env.FARO_KEY, 'base64pad'));
+            peerId = await createFromProtobuf(fromString(process.env.FARO_KEY, 'base64pad'));
         } catch (e) {
             console.error('❌ Error cargando FARO_KEY:', e.message);
         }
     }
 
     const node = await createLibp2p({
-        privateKey,
+        peerId,
         addresses: {
             listen: [
                 `/ip4/0.0.0.0/tcp/${port}/ws`
